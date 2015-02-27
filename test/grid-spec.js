79f714f17b5ebc8edbdc92d40c49c1fcf9e0900b
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2015-02-27 11:38:11 +0100

    refactor: factorize test using sort

--- test/grid-spec.js
+++ test/grid-spec.js
@@ -314,358 +314,268 @@ describe('Grid', function() {
     });
   });
 
-  it('should not sort grid by default', function() {
-    var columns = [
-      { id: 'id', title: 'Foo' },
-      { id: 'name', title: 'Boo' }
-    ];
+  describe('sort', function() {
+    var columns, data, table, grid;
 
-    // Data is not sorted
-    var data = [
-      { id: 2, name: 'foo2 '},
-      { id: 1, name: 'foo1 '},
-      { id: 3, name: 'foo3 '}
-    ];
+    beforeEach(function() {
+      columns = [
+        { id: 'id', sortable: false },
+        { id: 'firstName' },
+        { id: 'lastName' }
+      ];
 
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
+      data = [
+        { id: 2, firstName: 'foo2', lastName: 'bar2' },
+        { id: 1, firstName: 'foo1', lastName: 'bar1' },
+        { id: 3, firstName: 'foo2', lastName: 'bar3' }
+      ];
 
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
+      table = document.createElement('table');
+      fixtures.appendChild(table);
+
+      grid = new Grid(table, {
+        data: data,
+        columns: columns
+      });
     });
 
-     expect(grid.$sortBy).toEqual([]);
+    it('should not sort grid by default', function() {
+      expect(grid.$sortBy).toEqual([]);
 
-    var ths = grid.$thead[0].childNodes[0].childNodes;
+      var ths = grid.$thead[0].childNodes[0].childNodes;
 
-    expect(ths).toVerify(function(th) {
-      return th.getAttribute('data-waffle-order') === null;
+      expect(ths).toVerify(function(th) {
+        return th.getAttribute('data-waffle-order') === null;
+      });
+
+      expect(ths[0].className.split(' ')).not.toContain('waffle-sortable');
+      expect(ths[1].className.split(' ')).toContain('waffle-sortable');
+      expect(ths[2].className.split(' ')).toContain('waffle-sortable');
+
+      expect(ths).toVerify(function(th) {
+        return th.className.split(' ').indexOf('waffle-sortable-asc') < 0;
+      });
+
+      expect(ths).toVerify(function(th) {
+        return th.className.split(' ').indexOf('waffle-sortable-desc') < 0;
+      });
+
+      expect(grid.$data.toArray()).not.toBeSorted(function(o1, o2) {
+        return o1.id - o2.id;
+      });
+
+      expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
+        return tr.childNodes[0].innerHTML === data[idx].id.toString();
+      });
     });
 
-    expect(ths).toVerify(function(th) {
-      return th.className.split(' ').indexOf('waffle-sortable') >= 0;
+    it('should sort grid in ascendant order using one field', function() {
+      grid.sortBy('id');
+
+      expect(grid.$sortBy).toEqual(['+id']);
+
+      var ths = grid.$thead[0].childNodes[0].childNodes;
+      expect(ths[0].getAttribute('data-waffle-order')).toBe('+');
+      expect(ths[1].getAttribute('data-waffle-order')).toBeNull();
+
+      var classes0 = ths[0].className.split(' ');
+      expect(classes0).not.toContain('waffle-sortable');
+      expect(classes0).toContain('waffle-sortable-asc');
+      expect(classes0).not.toContain('waffle-sortable-desc');
+
+      var classes1 = ths[1].className.split(' ');
+      expect(classes1).toContain('waffle-sortable');
+      expect(classes1).not.toContain('waffle-sortable-asc');
+      expect(classes1).not.toContain('waffle-sortable-desc');
+
+      expect(grid.$data.toArray()).toBeSorted(function(o1, o2) {
+        return o1.id - o2.id;
+      });
+
+      expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
+        return tr.childNodes[0].innerHTML === grid.$data[idx].id.toString();
+      });
     });
 
-    expect(ths).toVerify(function(th) {
-      return th.className.split(' ').indexOf('waffle-sortable-asc') < 0;
+    it('should sort grid in descendant order using one field', function() {
+      grid.sortBy('-id');
+
+      expect(grid.$sortBy).toEqual(['-id']);
+
+      var ths = grid.$thead[0].childNodes[0].childNodes;
+      expect(ths[0].getAttribute('data-waffle-order')).toBe('-');
+      expect(ths[1].getAttribute('data-waffle-order')).toBeNull();
+
+      var classes0 = ths[0].className.split(' ');
+      expect(classes0).not.toContain('waffle-sortable');
+      expect(classes0).toContain('waffle-sortable-desc');
+      expect(classes0).not.toContain('waffle-sortable-asc');
+
+      var classes1 = ths[1].className.split(' ');
+      expect(classes1).toContain('waffle-sortable');
+      expect(classes1).not.toContain('waffle-sortable-asc');
+      expect(classes1).not.toContain('waffle-sortable-desc');
+
+      expect(grid.$data.toArray()).toBeSorted(function(o1, o2) {
+        return (o1.id - o2.id) * -1;
+      });
+
+      expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
+        return tr.childNodes[0].innerHTML === grid.$data[idx].id.toString();
+      });
     });
 
-    expect(ths).toVerify(function(th) {
-      return th.className.split(' ').indexOf('waffle-sortable-desc') < 0;
+    it('should sort grid in ascendant using two fields', function() {
+      grid.sortBy(['firstName', '-lastName']);
+
+      expect(grid.$sortBy).toEqual(['+firstName', '-lastName']);
+
+      var ths = grid.$thead[0].childNodes[0].childNodes;
+      expect(ths[0].getAttribute('data-waffle-order')).toBeNull();
+      expect(ths[1].getAttribute('data-waffle-order')).toBe('+');
+      expect(ths[2].getAttribute('data-waffle-order')).toBe('-');
+
+      var classes0 = ths[1].className.split(' ');
+      expect(classes0).toContain('waffle-sortable');
+      expect(classes0).toContain('waffle-sortable-asc');
+      expect(classes0).not.toContain('waffle-sortable-desc');
+
+      var classes1 = ths[2].className.split(' ');
+      expect(classes1).toContain('waffle-sortable');
+      expect(classes1).toContain('waffle-sortable-desc');
+      expect(classes1).not.toContain('waffle-sortable-asc');
+
+      expect(grid.$data.toArray()).toBeSorted(function(o1, o2) {
+        return (o1.firstName.localeCompare(o2.firstName)) ||
+               (o1.lastName.localeCompare(o2.lastName) * -1);
+      });
+
+      expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
+        return tr.childNodes[2].innerHTML === grid.$data[idx].lastName.toString();
+      });
     });
 
-    expect(grid.$data.toArray()).not.toBeSorted(function(o1, o2) {
-      return o1.id - o2.id;
+    it('should sort data when column header is clicked', function() {
+      spyOn(grid, 'sortBy').and.callThrough();
+
+      // Trigger click
+      var ths = grid.$thead[0].childNodes[0].childNodes;
+      var evt1 = document.createEvent('MouseEvent');
+      evt1.initEvent('click', true, true);
+      ths[1].dispatchEvent(evt1);
+
+      expect(grid.sortBy).toHaveBeenCalledWith(['+firstName']);
+      expect(grid.$sortBy).toEqual(['+firstName']);
+
+      grid.sortBy.calls.reset();
+
+      // New click should reverse order
+      var evt2 = document.createEvent('MouseEvent');
+      evt2.initEvent('click', true, true);
+      ths[1].dispatchEvent(evt2);
+
+      // Th should have flag
+      expect(grid.sortBy).toHaveBeenCalledWith(['-firstName']);
+      expect(grid.$sortBy).toEqual(['-firstName']);
     });
 
-    expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
-      return tr.childNodes[0].innerHTML === data[idx].id.toString();
-    });
-  });
+    it('should sort data when column header is clicked and column is not sortable', function() {
+      spyOn(grid, 'sortBy').and.callThrough();
 
-  it('should sort grid in ascendant order using one field', function() {
-    var columns = [
-      { id: 'id', title: 'Foo' },
-      { id: 'name', title: 'Boo' }
-    ];
+      // Trigger click
+      var ths = grid.$thead[0].childNodes[0].childNodes;
+      var evt1 = document.createEvent('MouseEvent');
+      evt1.initEvent('click', true, true);
+      ths[0].dispatchEvent(evt1);
 
-    // Data is not sorted
-    var data = [
-      { id: 2, name: 'foo2 '},
-      { id: 1, name: 'foo1 '},
-      { id: 3, name: 'foo3 '}
-    ];
-
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
-
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
+      expect(grid.sortBy).not.toHaveBeenCalled();
+      expect(grid.$sortBy).toEqual([]);
     });
 
-    grid.sortBy('id');
+    it('should sort data when column header is clicked using two field if shift key is pressed', function() {
+      var columns = [
+        { id: 'id', title: 'Foo' },
+        { id: 'name', title: 'Boo' }
+      ];
 
-    expect(grid.$sortBy).toEqual(['+id']);
+      var data = [
+        { id: 2, name: 'foo2 '},
+        { id: 1, name: 'foo1 '},
+        { id: 3, name: 'foo3 '}
+      ];
 
-    var ths = grid.$thead[0].childNodes[0].childNodes;
-    expect(ths[0].getAttribute('data-waffle-order')).toBe('+');
-    expect(ths[1].getAttribute('data-waffle-order')).toBeNull();
+      var table = document.createElement('table');
+      fixtures.appendChild(table);
 
-    var classes0 = ths[0].className.split(' ');
-    expect(classes0).toContain('waffle-sortable');
-    expect(classes0).toContain('waffle-sortable-asc');
-    expect(classes0).not.toContain('waffle-sortable-desc');
+      var grid = new Grid(table, {
+        data: data,
+        columns: columns
+      });
 
-    var classes1 = ths[1].className.split(' ');
-    expect(classes1).toContain('waffle-sortable');
-    expect(classes1).not.toContain('waffle-sortable-asc');
-    expect(classes1).not.toContain('waffle-sortable-desc');
+      spyOn(grid, 'sortBy').and.callThrough();
 
-    expect(grid.$data.toArray()).toBeSorted(function(o1, o2) {
-      return o1.id - o2.id;
+      // Trigger click
+      var ths = grid.$thead[0].childNodes[0].childNodes;
+      var evt1 = document.createEvent('MouseEvent');
+      evt1.initEvent('click', true, true);
+      ths[0].dispatchEvent(evt1);
+
+      expect(grid.sortBy).toHaveBeenCalledWith(['+id']);
+      expect(grid.$sortBy).toEqual(['+id']);
+
+      grid.sortBy.calls.reset();
+
+      // New click should reverse order
+      var evt2 = document.createEvent('MouseEvent');
+      evt2.initMouseEvent(
+          'click',    // type
+          true,       // canBubble
+          true,       // cancelable,
+          window,     // 'view'
+          0,          // detail
+          0,          // screenX,
+          0,          // screenY,
+          0,          // clientX,
+          0,          // clientY,
+          false,      // ctrlKey,
+          false,      // altKey,
+          true,       // shiftKey,
+          false,      // metaKey,
+          'left',     // button,
+          ths         // relatedTarget
+      );
+
+      ths[1].dispatchEvent(evt2);
+
+      expect(grid.sortBy).toHaveBeenCalledWith(['+id', '+name']);
+      expect(grid.$sortBy).toEqual(['+id', '+name']);
+
+      grid.sortBy.calls.reset();
+
+      // New click on id should reverse order of id column
+      var evt3 = document.createEvent('MouseEvent');
+      evt3.initMouseEvent(
+          'click',    // type
+          true,       // canBubble
+          true,       // cancelable,
+          window,     // 'view'
+          0,          // detail
+          0,          // screenX,
+          0,          // screenY,
+          0,          // clientX,
+          0,          // clientY,
+          false,      // ctrlKey,
+          false,      // altKey,
+          true,       // shiftKey,
+          false,      // metaKey,
+          'left',     // button,
+          ths         // relatedTarget
+      );
+
+      ths[0].dispatchEvent(evt3);
+      expect(grid.sortBy).toHaveBeenCalledWith(['+name', '-id']);
+      expect(grid.$sortBy).toEqual(['+name', '-id']);
     });
 
-    expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
-      return tr.childNodes[0].innerHTML === grid.$data[idx].id.toString();
-    });
-  });
-
-  it('should sort grid in descendant order using one field', function() {
-    var columns = [
-      { id: 'id', title: 'Foo' },
-      { id: 'name', title: 'Boo' }
-    ];
-
-    // Data is not sorted
-    var data = [
-      { id: 2, name: 'foo2 '},
-      { id: 1, name: 'foo1 '},
-      { id: 3, name: 'foo3 '}
-    ];
-
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
-
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
-    });
-
-    grid.sortBy('-id');
-
-    expect(grid.$sortBy).toEqual(['-id']);
-
-    var ths = grid.$thead[0].childNodes[0].childNodes;
-    expect(ths[0].getAttribute('data-waffle-order')).toBe('-');
-    expect(ths[1].getAttribute('data-waffle-order')).toBeNull();
-
-    var classes0 = ths[0].className.split(' ');
-    expect(classes0).toContain('waffle-sortable');
-    expect(classes0).toContain('waffle-sortable-desc');
-    expect(classes0).not.toContain('waffle-sortable-asc');
-
-    var classes1 = ths[1].className.split(' ');
-    expect(classes1).toContain('waffle-sortable');
-    expect(classes1).not.toContain('waffle-sortable-asc');
-    expect(classes1).not.toContain('waffle-sortable-desc');
-
-    expect(grid.$data.toArray()).toBeSorted(function(o1, o2) {
-      return (o1.id - o2.id) * -1;
-    });
-
-    expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
-      return tr.childNodes[0].innerHTML === grid.$data[idx].id.toString();
-    });
-  });
-
-  it('should sort grid in ascendant using two fields', function() {
-    var columns = [
-      { id: 'firstName' },
-      { id: 'lastName' }
-    ];
-
-    // Data is not sorted
-    var data = [
-      { id: 2, firstName: 'foo2', lastName: 'bar2' },
-      { id: 1, firstName: 'foo1', lastName: 'bar1' },
-      { id: 3, firstName: 'foo2', lastName: 'bar3' }
-    ];
-
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
-
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
-    });
-
-    grid.sortBy(['firstName', '-lastName']);
-
-    expect(grid.$sortBy).toEqual(['+firstName', '-lastName']);
-
-    var ths = grid.$thead[0].childNodes[0].childNodes;
-    expect(ths[0].getAttribute('data-waffle-order')).toBe('+');
-    expect(ths[1].getAttribute('data-waffle-order')).toBe('-');
-
-    var classes0 = ths[0].className.split(' ');
-    expect(classes0).toContain('waffle-sortable');
-    expect(classes0).toContain('waffle-sortable-asc');
-    expect(classes0).not.toContain('waffle-sortable-desc');
-
-    var classes1 = ths[1].className.split(' ');
-    expect(classes1).toContain('waffle-sortable');
-    expect(classes1).toContain('waffle-sortable-desc');
-    expect(classes1).not.toContain('waffle-sortable-asc');
-
-    expect(grid.$data.toArray()).toBeSorted(function(o1, o2) {
-      return (o1.firstName.localeCompare(o2.firstName)) ||
-             (o1.lastName.localeCompare(o2.lastName) * -1);
-    });
-
-    expect(grid.$tbody[0].childNodes).toVerify(function(tr, idx) {
-      return tr.childNodes[1].innerHTML === grid.$data[idx].lastName.toString();
-    });
-  });
-
-  it('should sort data when column header is clicked', function() {
-    var columns = [
-      { id: 'id', title: 'Foo' },
-      { id: 'name', title: 'Boo' }
-    ];
-
-    var data = [
-      { id: 2, name: 'foo2 '},
-      { id: 1, name: 'foo1 '},
-      { id: 3, name: 'foo3 '}
-    ];
-
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
-
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
-    });
-
-    spyOn(grid, 'sortBy').and.callThrough();
-
-    // Trigger click
-    var ths = grid.$thead[0].childNodes[0].childNodes;
-    var evt1 = document.createEvent('MouseEvent');
-    evt1.initEvent('click', true, true);
-    ths[0].dispatchEvent(evt1);
-
-    expect(grid.sortBy).toHaveBeenCalledWith(['+id']);
-    expect(grid.$sortBy).toEqual(['+id']);
-
-    grid.sortBy.calls.reset();
-
-    // New click should reverse order
-    var evt2 = document.createEvent('MouseEvent');
-    evt2.initEvent('click', true, true);
-    ths[0].dispatchEvent(evt2);
-
-    // Th should have flag
-    expect(grid.sortBy).toHaveBeenCalledWith(['-id']);
-    expect(grid.$sortBy).toEqual(['-id']);
-  });
-
-  it('should sort data when column header is clicked and column is not sortable', function() {
-    var columns = [
-      { id: 'id', title: 'Foo', sortable: false },
-      { id: 'name', title: 'Boo' }
-    ];
-
-    var data = [
-      { id: 2, name: 'foo2 '},
-      { id: 1, name: 'foo1 '},
-      { id: 3, name: 'foo3 '}
-    ];
-
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
-
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
-    });
-
-    spyOn(grid, 'sortBy').and.callThrough();
-
-    // Trigger click
-    var ths = grid.$thead[0].childNodes[0].childNodes;
-    var evt1 = document.createEvent('MouseEvent');
-    evt1.initEvent('click', true, true);
-    ths[0].dispatchEvent(evt1);
-
-    expect(grid.sortBy).not.toHaveBeenCalled();
-    expect(grid.$sortBy).toEqual([]);
-  });
-
-  it('should sort data when column header is clicked using two field if shift key is pressed', function() {
-    var columns = [
-      { id: 'id', title: 'Foo' },
-      { id: 'name', title: 'Boo' }
-    ];
-
-    var data = [
-      { id: 2, name: 'foo2 '},
-      { id: 1, name: 'foo1 '},
-      { id: 3, name: 'foo3 '}
-    ];
-
-    var table = document.createElement('table');
-    fixtures.appendChild(table);
-
-    var grid = new Grid(table, {
-      data: data,
-      columns: columns
-    });
-
-    spyOn(grid, 'sortBy').and.callThrough();
-
-    // Trigger click
-    var ths = grid.$thead[0].childNodes[0].childNodes;
-    var evt1 = document.createEvent('MouseEvent');
-    evt1.initEvent('click', true, true);
-    ths[0].dispatchEvent(evt1);
-
-    expect(grid.sortBy).toHaveBeenCalledWith(['+id']);
-    expect(grid.$sortBy).toEqual(['+id']);
-
-    grid.sortBy.calls.reset();
-
-    // New click should reverse order
-    var evt2 = document.createEvent('MouseEvent');
-    evt2.initMouseEvent(
-        'click',    // type
-        true,       // canBubble
-        true,       // cancelable,
-        window,     // 'view'
-        0,          // detail
-        0,          // screenX,
-        0,          // screenY,
-        0,          // clientX,
-        0,          // clientY,
-        false,      // ctrlKey,
-        false,      // altKey,
-        true,       // shiftKey,
-        false,      // metaKey,
-        'left',     // button,
-        ths         // relatedTarget
-    );
-
-    ths[1].dispatchEvent(evt2);
-
-    expect(grid.sortBy).toHaveBeenCalledWith(['+id', '+name']);
-    expect(grid.$sortBy).toEqual(['+id', '+name']);
-
-    grid.sortBy.calls.reset();
-
-    // New click on id should reverse order of id column
-    var evt3 = document.createEvent('MouseEvent');
-    evt3.initMouseEvent(
-        'click',    // type
-        true,       // canBubble
-        true,       // cancelable,
-        window,     // 'view'
-        0,          // detail
-        0,          // screenX,
-        0,          // screenY,
-        0,          // clientX,
-        0,          // clientY,
-        false,      // ctrlKey,
-        false,      // altKey,
-        true,       // shiftKey,
-        false,      // metaKey,
-        'left',     // button,
-        ths         // relatedTarget
-    );
-
-    ths[0].dispatchEvent(evt3);
-    expect(grid.sortBy).toHaveBeenCalledWith(['+name', '-id']);
-    expect(grid.$sortBy).toEqual(['+name', '-id']);
   });
 });

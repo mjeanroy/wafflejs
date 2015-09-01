91a4fec430c7715c15bf1624ed40d357e8ae1d62
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2015-09-01 13:46:24 +0200

    refacto: avoid duplication

--- src/core/services/comparators.js
+++ src/core/services/comparators.js
@@ -27,64 +27,87 @@
 /* exported $comparators */
 /* exported $$createComparisonFunction */
 
-var $comparators = {
-  // Compare two strings
-  $string: function(a, b) {
-    var str1 = a == null ? '' : String(a);
-    var str2 = b == null ? '' : String(b);
-    return str1.localeCompare(str2);
-  },
+var $comparators = (function() {
 
-  // Compare two numbers
-  $number: function(a, b) {
-    var n1 = a == null ? 0 : Number(a);
-    var n2 = b == null ? 0 : Number(b);
-    return n1 - n2;
-  },
+  // Convert an object to a string.
+  var toString = function(o) {
+    return o == null ? '' : String(o.toString());
+  };
 
-  // Compare two booleans
-  $boolean: function(a, b) {
-    var b1 = a === 'false' ? 0 : (Boolean(a) ? 1 : 0);
-    var b2 = b === 'false' ? 0 : (Boolean(b) ? 1 : 0);
-    return b1 - b2;
-  },
+  // Convert an object to a number.
+  // NaN is automatically converted to zero.
+  var toNumber = function(o) {
+    return o == null ? 0 : (Number(o) || 0);
+  };
 
-  // Compare two dates
-  // Function accept timestamps as arguments
-  $date: function(a, b) {
-    var t1 = _.isDate(a) ? a.getTime() : new Date(a == null ? 0 : a).getTime();
-    var t2 = _.isDate(b) ? b.getTime() : new Date(b == null ? 0 : b).getTime();
-    return t1 - t2;
-  },
+  // Convert an object to a boolean integer.
+  // A boolean integer is zero for a falsy value, one for a truthy value.
+  // String "false" will be converted to a falsy boolean.
+  var toBoolean = function(o) {
+    return o === 'false' ? 0 : (Boolean(o) ? 1 : 0);
+  };
 
-  // Perform an automatic comparison
-  // Type of elements will be detected and appropriate comparison will be made
-  $auto: function(a, b) {
-    if (a === b || (a == null && b == null)) {
-      return 0;
+  // Convert an object to a timestamp.
+  var toDate = function(o) {
+    var d = _.isDate(o) ? o : new Date(toNumber(o));
+    return d.getTime();
+  };
+
+  // Create a comparison function by using substract operator.
+  // First argument is a factory to transform arguments to a mathemical value.
+  var toSubstraction = function(factory) {
+    return function(a, b) {
+      return factory(a) - factory(b);
+    };
+  };
+
+  var instance = {
+    // Compare two strings
+    $string: function(a, b) {
+      return toString(a).localeCompare(toString(b));
+    },
+
+    // Compare two numbers
+    $number: toSubstraction(toNumber),
+
+    // Compare two booleans
+    $boolean: toSubstraction(toBoolean),
+
+    // Compare two dates
+    // Function accept timestamps as arguments
+    $date: toSubstraction(toDate),
+
+    // Perform an automatic comparison
+    // Type of elements will be detected and appropriate comparison will be made
+    $auto: function(a, b) {
+      if (a === b || (a == null && b == null)) {
+        return 0;
+      }
+
+      // Try to get most precise type
+      // String must be always at last because it is the less precise type
+      var value = _.find(['Number', 'Boolean', 'Date', 'String'], function(val) {
+        var fn = _['is' + val];
+        return fn(a) || fn(b);
+      });
+
+      if (value) {
+        return instance['$' + value.toLowerCase()](a, b);
+      }
+
+      // Just do a simple comparison... (strict equality is already checked)
+      return a < b ? -1 : 1;
+    },
+
+    // Get comparator by its name
+    // Can be overridden by custom lookup
+    $get: function(name) {
+      return instance[name];
     }
+  };
 
-    // Try to get most precise type
-    // String must be always at last because it is the less precise type
-    var value = _.find(['Number', 'Boolean', 'Date', 'String'], function(val) {
-      var fn = _['is' + val];
-      return fn(a) || fn(b);
-    });
-
-    if (value) {
-      return $comparators['$' + value.toLowerCase()](a, b);
-    }
-
-    // Just do a simple comparison... (strict equality is already checked)
-    return a < b ? -1 : 1;
-  },
-
-  // Get comparator by its name
-  // Can be overridden by custom lookup
-  $get: function(name) {
-    return $comparators[name];
-  }
-};
+  return instance;
+})();
 
 // Create a comparison function using array of comparator
 // Comparison function take two object in parameters and iterate

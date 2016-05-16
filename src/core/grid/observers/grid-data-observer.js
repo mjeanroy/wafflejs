dd9d284332d4c0a8c620b83de40b024ef4a1f1be
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2016-05-16 23:55:40 +0200

    refactor: migrate GridDataObserver to es6

--- src/core/grid/observers/grid-data-observer.js
+++ src/core/grid/observers/grid-data-observer.js
@@ -40,52 +40,53 @@
  * - When data is updated, associated dom row are updated.
  */
 
-var GridDataObserver = (function() {
-  var readDataIndex = GridUtil.getDataIndex;
-  var findPreviousIndex = GridUtil.getPreviousRowIndexForDataIndex;
-  var findIndex = GridUtil.getRowIndexForDataIndex;
+const GridDataObserver = (() => {
+  const readDataIndex = GridUtil.getDataIndex;
+  const findPreviousIndex = GridUtil.getPreviousRowIndexForDataIndex;
+  const findIndex = GridUtil.getRowIndexForDataIndex;
 
-  var instance = {
+  const instance = {
     // Apply data changes to grid.
     on: function(changes) {
-      _.forEach(changes, function(change) {
-        var fnName = 'on' + $util.capitalize(change.type);
+      _.forEach(changes, change => {
+        const fnName = 'on' + $util.capitalize(change.type);
         instance[fnName].call(this, change);
-      }, this);
+      });
 
       return this;
     },
 
     // Update grid on splice change.
     onSplice: function(change) {
-      var $tbody = this.$tbody;
-      var tbody = $tbody[0];
-      var childNodes = tbody.childNodes;
-      var index = change.index;
-      var addedCount = change.addedCount;
-      var collection = change.object;
+      const $tbody = this.$tbody;
+      const tbody = $tbody[0];
+      const childNodes = tbody.childNodes;
+      const index = change.index;
+      const addedCount = change.addedCount;
+      const collection = change.object;
 
-      var removedNodes;
-      var addedNodes;
-      var addedData = change.added;
-      var removedData = change.removed;
-      var startIndex = findPreviousIndex(childNodes, index) + 1;
+      const addedData = change.added;
+      const removedData = change.removed;
+      const startIndex = findPreviousIndex(childNodes, index) + 1;
 
-      var removedCount = removedData.length;
+      let removedNodes;
+      let addedNodes;
+
+      const removedCount = removedData.length;
       if (removedCount > 0) {
-        var wasCleared = index === 0 && (collection.length - addedCount) === 0;
+        const wasCleared = index === 0 && (collection.length - addedCount) === 0;
         if (wasCleared) {
           removedNodes = _.toArray(childNodes);
           $tbody.empty();
         } else {
           removedNodes = [];
 
-          var removedRow = childNodes[startIndex];
+          let removedRow = childNodes[startIndex];
 
-          for (var k = 0; k < removedCount && removedRow; ++k) {
+          for (let k = 0; k < removedCount && removedRow; ++k) {
             // Remove if and only if row index match
-            var currentRemovedIndex = index + k;
-            var currentRowIndex = readDataIndex(removedRow);
+            const currentRemovedIndex = index + k;
+            const currentRowIndex = readDataIndex(removedRow);
             if (currentRowIndex === currentRemovedIndex) {
               removedNodes.push(tbody.removeChild(removedRow));
               removedRow = childNodes[startIndex];
@@ -94,7 +95,7 @@ var GridDataObserver = (function() {
         }
 
         // Do not forget to remove previously selected data !
-        var $selection = this.$selection;
+        const $selection = this.$selection;
         if ($selection && $selection.length > 0) {
           $selection.remove(removedData);
         }
@@ -104,20 +105,20 @@ var GridDataObserver = (function() {
       if (addedCount > 0) {
         addedNodes = [];
 
-        var fragment = $doc.createFragment();
-        var predicate = this.$filter || _.constant(true);
+        const fragment = $doc.createFragment();
+        const predicate = this.$filter || _.constant(true);
 
-        for (var i = 0; i < addedCount; ++i) {
-          var rowIdx = i + index;
-          var data = addedData[i];
-          var ctx = collection.ctx(data);
+        for (let i = 0; i < addedCount; ++i) {
+          const rowIdx = i + index;
+          const data = addedData[i];
+          const ctx = collection.ctx(data);
 
           // Update flag
           ctx.visible = predicate(data);
 
           // Append if and only if data must be visible
           if (ctx.visible) {
-            var tr = GridBuilder.tbodyRow(this, data, rowIdx);
+            const tr = GridBuilder.tbodyRow(this, data, rowIdx);
             addedNodes.push(tr);
             fragment.appendChild(tr);
           }
@@ -136,12 +137,12 @@ var GridDataObserver = (function() {
 
       if (removedNodes || addedNodes) {
         // We need to update row index
-        var diff = addedCount - removedCount;
-        var start = index + _.size(addedNodes);
+        const diff = addedCount - removedCount;
 
-        for (var length = childNodes.length; start < length; ++start) {
-          var childNode = childNodes[start];
-          var oldIdx = readDataIndex(childNode);
+        let start = index + _.size(addedNodes);
+        for (let length = childNodes.length; start < length; ++start) {
+          const childNode = childNodes[start];
+          const oldIdx = readDataIndex(childNode);
           childNode.setAttribute(DATA_WAFFLE_IDX, oldIdx + diff);
         }
 
@@ -161,17 +162,17 @@ var GridDataObserver = (function() {
 
     // Update grid on update change
     onUpdate: function(change) {
-      var index = change.index;
-      var data = this.$data.at(index);
+      const index = change.index;
+      const data = this.$data.at(index);
 
       // Create new node representation and merge diff with old node
-      var tbody = this.$tbody[0];
-      var childNodes = tbody.childNodes;
-      var nodeIndex = findIndex(childNodes, index);
+      const tbody = this.$tbody[0];
+      const childNodes = tbody.childNodes;
+      const nodeIndex = findIndex(childNodes, index);
       if (nodeIndex >= 0) {
-        var node = childNodes[nodeIndex];
-        var tmpNode = GridBuilder.tbodyRow(this, data, index);
-        var cid = node.getAttribute(DATA_WAFFLE_CID);
+        const node = childNodes[nodeIndex];
+        const tmpNode = GridBuilder.tbodyRow(this, data, index);
+        const cid = node.getAttribute(DATA_WAFFLE_CID);
 
         // Merge new row into old row (this will update content).
         $vdom.mergeNodes(tbody, node, tmpNode);

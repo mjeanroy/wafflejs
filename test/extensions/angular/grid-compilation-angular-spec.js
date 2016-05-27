31754dcb0e17ff2948d5bcb4968c5e09270eafb0
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2016-05-27 18:06:41 +0200

    refactor(test): migrate grid-compilation-angular-spec to es6

--- test/extensions/angular/grid-compilation-angular-spec.js
+++ test/extensions/angular/grid-compilation-angular-spec.js
@@ -22,78 +22,78 @@
  * SOFTWARE.
  */
 
-describe('Waffle NgCompilator', function() {
+describe('Waffle NgCompilator', () => {
 
-  var $rootScope;
-  var $compile;
+  let $rootScope;
+  let $compile;
 
-  beforeEach(inject(function(_$rootScope_, _$compile_) {
+  beforeEach(inject((_$rootScope_, _$compile_) => {
     $rootScope = _$rootScope_;
     $compile = _$compile_;
   }));
 
-  it('should compile a row', function() {
-    var index = 0;
-    var data = {
+  it('should compile a row', () => {
+    const index = 0;
+    const data = {
       id: 1,
       name: 'foo'
     };
 
-    var parentScope = $rootScope.$new();
+    const parentScope = $rootScope.$new();
 
-    var tr = document.createElement('tr');
+    const tr = document.createElement('tr');
     tr.setAttribute('data-waffle-cid', 1);
 
     spyOn(parentScope, '$new').and.callThrough();
 
-    var result = NgCompilator.compile(parentScope, tr, data, index);
+    const result = NgCompilator.compile(parentScope, tr, data, index);
 
     expect(result).toBeDefined();
     expect(result).toBe(tr);
     expect(parentScope.$new).toHaveBeenCalled();
 
-    var $scope = angular.element(tr).scope();
+    const $scope = angular.element(tr).scope();
     expect($scope).toBeDefined();
     expect($scope.$parent).toBe(parentScope);
     expect($scope.$data).toBe(data);
     expect($scope.$index).toBe(index);
   });
 
-  it('should compile a row and destroy old scope', function() {
-    var index = 0;
-    var data = {
+  it('should compile a row and destroy old scope', () => {
+    const index = 0;
+    const data = {
       id: 1,
       name: 'foo'
     };
 
-    var parentScope = $rootScope.$new();
+    const parentScope = $rootScope.$new();
 
-    var tr = document.createElement('tr');
+    const tr = document.createElement('tr');
     tr.setAttribute('data-waffle-cid', 1);
 
     spyOn(parentScope, '$new').and.callThrough();
 
-    var r1 = NgCompilator.compile(parentScope, tr, data, index);
+    const r1 = NgCompilator.compile(parentScope, tr, data, index);
 
     expect(r1).toBeDefined();
     expect(r1).toBe(tr);
     expect(parentScope.$new).toHaveBeenCalled();
 
-    var $scope1 = angular.element(tr).scope();
+    const $scope1 = angular.element(tr).scope();
     expect($scope1).toBeDefined();
     expect($scope1.$parent).toBe(parentScope);
     expect($scope1.$data).toBe(data);
     expect($scope1.$index).toBe(index);
 
-    var destroy = spyOn($scope1, '$destroy').and.callThrough();
+    const destroy = spyOn($scope1, '$destroy').and.callThrough();
 
-    var r2 = NgCompilator.compile(parentScope, tr, data, index);
+    const r2 = NgCompilator.compile(parentScope, tr, data, index);
 
     expect(r2).toBeDefined();
     expect(r2).toBe(r1);
     expect(destroy).toHaveBeenCalled();
 
-    var $scope2 = angular.element(r2).scope();
+    const $scope2 = angular.element(r2).scope();
     expect($scope2).toBeDefined();
     expect($scope2.$parent).toBe(parentScope);
     expect($scope2).not.toBe($scope1);
@@ -101,118 +101,118 @@ describe('Waffle NgCompilator', function() {
     expect($scope2.$index).toBe(index);
   });
 
-  it('should destroy scope using row', function() {
-    var index = 0;
-    var data = {
+  it('should destroy scope using row', () => {
+    const index = 0;
+    const data = {
       id: 1,
       name: 'foo'
     };
 
-    var parentScope = $rootScope.$new();
+    const parentScope = $rootScope.$new();
 
-    var tr = document.createElement('tr');
+    const tr = document.createElement('tr');
     tr.setAttribute('data-waffle-cid', 1);
 
     spyOn(parentScope, '$new').and.callThrough();
 
-    var r1 = NgCompilator.compile(parentScope, tr, data, index);
-    var $scope1 = angular.element(r1).scope();
+    const r1 = NgCompilator.compile(parentScope, tr, data, index);
+    const $scope1 = angular.element(r1).scope();
     expect($scope1.$$destroyed).toBeDefined();
     expect($scope1.$$destroyed).toBeFalse();
 
     NgCompilator.destroy(r1);
 
-    var $scope2 = angular.element(r1).scope();
+    const $scope2 = angular.element(r1).scope();
     expect($scope2).toBeDefined();
     expect($scope2.$$destroyed).toBeTrue();
   });
 
-  it('should destroy scope using cid', function() {
-    var index = 0;
-    var data = {
+  it('should destroy scope using cid', () => {
+    const index = 0;
+    const data = {
       id: 1,
       name: 'foo'
     };
 
-    var parentScope = $rootScope.$new();
+    const parentScope = $rootScope.$new();
 
-    var tr = document.createElement('tr');
+    const tr = document.createElement('tr');
     tr.setAttribute('data-waffle-cid', 1);
 
     spyOn(parentScope, '$new').and.callThrough();
 
-    var r1 = NgCompilator.compile(parentScope, tr, data, index);
-    var $scope1 = angular.element(r1).scope();
+    const r1 = NgCompilator.compile(parentScope, tr, data, index);
+    const $scope1 = angular.element(r1).scope();
     expect($scope1.$$destroyed).toBeDefined();
     expect($scope1.$$destroyed).toBeFalse();
 
     NgCompilator.destroy(1);
 
-    var $scope2 = angular.element(r1).scope();
+    const $scope2 = angular.element(r1).scope();
     expect($scope2).toBeDefined();
     expect($scope2.$$destroyed).toBeTrue();
   });
 
-  it('should refresh scope', function() {
-    var index = 0;
-    var data = {
+  it('should refresh scope', () => {
+    const index = 0;
+    const data = {
       id: 1,
       name: 'foo'
     };
 
-    var parentScope = $rootScope.$new();
+    const parentScope = $rootScope.$new();
 
-    var tr = document.createElement('tr');
+    const tr = document.createElement('tr');
     tr.setAttribute('data-waffle-cid', 1);
 
-    var r1 = NgCompilator.compile(parentScope, tr, data, index);
+    const r1 = NgCompilator.compile(parentScope, tr, data, index);
     expect(r1).toBeDefined();
     expect(r1).toBe(tr);
 
-    var $scope1 = angular.element(r1).scope();
+    const $scope1 = angular.element(r1).scope();
     expect($scope1).toBeDefined();
     expect($scope1.$$destroyed).toBeFalse();
 
-    var r2 = NgCompilator.refresh(tr);
+    const r2 = NgCompilator.refresh(tr);
     expect(r2).toBeDefined();
     expect(r2).toBe(r1);
 
-    var $scope2 = angular.element(r2).scope();
+    const $scope2 = angular.element(r2).scope();
     expect($scope2).toBeDefined();
     expect($scope2).not.toBe($scope1);
     expect($scope2.$$destroyed).toBeFalse();
     expect($scope1.$$destroyed).toBeTrue();
   });
 
-  describe('once initialized', function() {
-    var $scope;
-    var $table;
-    var table;
+  describe('once initialized', () => {
+    let $scope;
+    let $table;
+    let table;
 
     // Set of utility function
-    var getRows;
-    var getChildScopes;
-    var spyDestroyFn;
+    let getRows;
+    let getChildScopes;
+    let spyDestroyFn;
 
-    beforeEach(function() {
+    beforeEach(() => {
       $scope = $rootScope.$new();
 
-      var data = [
+      const data = [
         { id: 1, name: 'foo' },
         { id: 2, name: 'bar' },
         { id: 3, name: 'foobar' }
       ];
 
-      var columnActions = {
+      const columnActions = {
         id: 'actions',
         title: 'Actions',
         escape: false,
-        renderer: [function() {
+        renderer: [() => {
           return '<button ng-click="foo($data, $index)"></button>';
         }]
       };
 
-      var columns = [
+      const columns = [
         { id: 'id', title: 'Id' },
         { id: 'name', title: 'Name' },
         columnActions
@@ -228,12 +228,11 @@ describe('Waffle NgCompilator', function() {
         }
       };
 
-      var html = '' +
-        '<table waffle ' +
-        '       data-waffle-options="options" ' +
-        '       data-waffle-grid="grid" ' +
-        '       data-waffle-ng-compile="true" ' +
-        '></table>';
+      const html =
+        `<table waffle
+                data-waffle-options="options"
+                data-waffle-grid="grid"
+                data-waffle-ng-compile="true"></table>`;
 
       $table = $compile(html)($scope);
       table = $table[0];
@@ -241,54 +240,52 @@ describe('Waffle NgCompilator', function() {
       // Trigger a digest
       $scope.$digest();
 
-      getRows = function() {
-        var tbody = table.childNodes[1];
-        var childNodes = tbody.childNodes;
+      getRows = () => {
+        const tbody = table.childNodes[1];
+        const childNodes = tbody.childNodes;
 
-        var results = [];
+        const results = [];
 
-        for (var i = 0; i < childNodes.length; i++) {
+        for (let i = 0; i < childNodes.length; i++) {
           results.push(childNodes[i]);
         }
 
         return results;
       };
 
-      getChildScopes = function() {
-        var rows = getRows();
-        var childScopes = [];
+      getChildScopes = () => {
+        const rows = getRows();
+        const childScopes = [];
 
-        for (var i = 0; i < rows.length; i++) {
-          var scope = angular.element(rows[i]).scope();
+        for (let i = 0; i < rows.length; i++) {
+          const scope = angular.element(rows[i]).scope();
           childScopes.push(scope);
         }
 
         return childScopes;
       };
 
-      spyDestroyFn = function(scopes) {
-        var spies = [];
-        for (var i = 0; i < scopes.length; i++) {
+      spyDestroyFn = scopes => {
+        const spies = [];
+        for (let i = 0; i < scopes.length; i++) {
           spies.push(spyOn(scopes[i], '$destroy').and.callThrough());
         }
         return spies;
       };
     });
 
-    afterEach(function() {
-      $scope.$destroy();
-    });
+    afterEach(() => $scope.$destroy());
 
-    it('should handle child scopes when grid is initialized', function() {
+    it('should handle child scopes when grid is initialized', () => {
       expect($scope.grid.options.ng).toEqual({
         $scope: $scope
       });
 
-      var childNodes = $table[0].childNodes;
-      var tbody = childNodes[1];
-      var tr0 = tbody.childNodes[0];
-      var button = tr0.childNodes[3].childNodes[0];
-      var $button = angular.element(button);
+      const childNodes = $table[0].childNodes;
+      const tbody = childNodes[1];
+      const tr0 = tbody.childNodes[0];
+      const button = tr0.childNodes[3].childNodes[0];
+      const $button = angular.element(button);
 
       expect($scope.foo).not.toHaveBeenCalled();
 
@@ -298,24 +295,22 @@ describe('Waffle NgCompilator', function() {
       expect($scope.foo).toHaveBeenCalledWith($scope.grid.data().at(0), 0);
     });
 
-    it('should destroy old scopes when grid is rendered', function() {
-      var spies = spyDestroyFn(getChildScopes());
+    it('should destroy old scopes when grid is rendered', () => {
+      const spies = spyDestroyFn(getChildScopes());
 
       // Render tbody
       $scope.grid.renderBody();
 
-      expect(spies).toVerify(function(spy) {
-        return spy.calls.count() === 1;
-      });
+      expect(spies).toVerify(spy => spy.calls.count() === 1);
     });
 
-    it('should destroy old scopes when data are removed', function() {
+    it('should destroy old scopes when data are removed', () => {
       spyOn(NgCompilator, 'destroy').and.callThrough();
 
-      var rows = getRows();
-      var lastRow = rows[rows.length - 1];
-      var scope = angular.element(lastRow).scope();
-      var spy = spyOn(scope, '$destroy').and.callThrough();
+      const rows = getRows();
+      const lastRow = rows[rows.length - 1];
+      const scope = angular.element(lastRow).scope();
+      const spy = spyOn(scope, '$destroy').and.callThrough();
 
       // Remove last row
       $scope.grid.data().pop();
@@ -328,14 +323,11 @@ describe('Waffle NgCompilator', function() {
       expect(spy).toHaveBeenCalled();
     });
 
-    it('should destroy old scopes when data are filtered', function() {
+    it('should destroy old scopes when data are filtered', () => {
       spyOn(NgCompilator, 'destroy').and.callThrough();
 
-      var oddPredicate = jasmine.createSpy('oddPredicate').and.callFake(function(o) {
-        return o.id % 2 === 0;
-      });
-
-      var destroyFns = spyDestroyFn(getChildScopes());
+      const oddPredicate = jasmine.createSpy('oddPredicate').and.callFake(o => o.id % 2 === 0);
+      const destroyFns = spyDestroyFn(getChildScopes());
 
       $scope.grid.filter(oddPredicate);
 
@@ -344,8 +336,8 @@ describe('Waffle NgCompilator', function() {
       expect(destroyFns[2]).toHaveBeenCalled();
     });
 
-    it('it should re-create scope when columns are added', function() {
-      var spies = spyDestroyFn(getChildScopes());
+    it('it should re-create scope when columns are added', () => {
+      const spies = spyDestroyFn(getChildScopes());
 
       spyOn(NgCompilator, 'refresh').and.callThrough();
 
@@ -357,21 +349,17 @@ describe('Waffle NgCompilator', function() {
 
       expect(NgCompilator.refresh).toHaveBeenCalled();
 
-      expect(spies).toVerify(function(spy) {
-        return spy.calls.count() > 0;
-      });
+      expect(spies).toVerify(spy => spy.calls.count() > 0);
 
-      var newScopes = getChildScopes();
+      const newScopes = getChildScopes();
       expect(newScopes.length).toBe(3);
-      expect(newScopes).toVerify(function(scope) {
-        return !scope.$$destroyed;
-      });
+      expect(newScopes).toVerify(scope => !scope.$$destroyed);
     });
 
-    it('it should destroy scope of temporary nodes', function() {
-      var mergeNodes = $vdom.mergeNodes;
+    it('it should destroy scope of temporary nodes', () => {
+      const mergeNodes = $vdom.mergeNodes;
 
-      var scope = null;
+      let scope = null;
       spyOn($vdom, 'mergeNodes').and.callFake(function(parent, oldNode, tmpNode) {
         scope = angular.element(tmpNode).scope();
         return mergeNodes.apply(this, arguments);

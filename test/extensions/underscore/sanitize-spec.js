fb27204a8c91416fd0e127251caead2a79c6896c
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2016-05-27 18:14:00 +0200

    refactor(test): migrate sanitize-spec to es6

--- test/extensions/underscore/sanitize-spec.js
+++ test/extensions/underscore/sanitize-spec.js
@@ -22,14 +22,14 @@
  * SOFTWARE.
  */
 
-describe('underscore - sanitize', function() {
+describe('underscore - sanitize', () => {
 
-  it('should escape html string', function() {
-    var html = '<em onmouseover="this.textContent=\'PWN3D!\'">click here</em>';
+  it('should escape html string', () => {
+    const html = `<em onmouseover="this.textContent='PWN3D!'">click here</em>`;
     expect($sanitize(html)).toBe('&lt;em onmouseover=&quot;this.textContent=&#x27;PWN3D!&#x27;&quot;&gt;click here&lt;/em&gt;');
   });
 
-  it('should not change safe string', function() {
+  it('should not change safe string', () => {
     expect($sanitize('foo')).toBe('foo');
   });
 });
